464ce3bcf452104f3d033cea1213a2908e1126aa
roy845 <royatali94@gmail.com>
2023-07-27 23:02:37 +0300

    updated server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const routes = require("./routes");
 const morgan = require("morgan");
 const { connectDB } = require("./config/db");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
 
 //configure env
 dotenv.config();
@@ -18,9 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
-//routes
-app.use("/api/auth", authRoutes);
+app.use(routes);
 
 //PORT
 const PORT = process.env.PORT || 8080;
